6cb18e5010e1456b3f3a25bf9bcf94d51f30e61c
BohanWu <819186192@qq.com>
2024-03-05 16:39:41 -0600

    feat: framework

---
+++ main.py
@@ -0,0 +1,68 @@
+import asyncio
+import semantic_kernel as sk
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion, AzureChatCompletion
+
+kernel = sk.Kernel()
+
+# Prepare OpenAI service using credentials stored in the `.env` file
+api_key, org_id = sk.openai_settings_from_dot_env()
+service_id="chat-gpt"
+kernel.add_service(
+    OpenAIChatCompletion(
+        service_id=service_id,
+        ai_model_id="gpt-3.5-turbo",
+        api_key=api_key,
+        org_id=org_id
+    )
+)
+
+# Alternative using Azure:
+# deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
+# kernel.add_service(
+#   AzureChatCompletion(
+#       service_id="dv",
+#       deployment_name=deployment,
+#       base_url=endpoint,
+#       api_key=api_key
+#   )
+# )
+
+# Define the request settings
+req_settings = kernel.get_service(service_id).get_prompt_execution_settings_class()(service_id=service_id)
+req_settings.max_tokens = 2000
+req_settings.temperature = 0.7
+req_settings.top_p = 0.8
+
+prompt = """
+1) A robot may not injure a human being or, through inaction,
+allow a human being to come to harm.
+
+2) A robot must obey orders given it by human beings except where
+such orders would conflict with the First Law.
+
+3) A robot must protect its own existence as long as such protection
+does not conflict with the First or Second Law.
+
+Give me the TLDR in exactly 5 words."""
+
+prompt_template_config = sk.PromptTemplateConfig(
+    template=prompt,
+    name="tldr",
+    template_format="semantic-kernel",
+    execution_settings=req_settings,
+)
+
+function = kernel.create_function_from_prompt(
+    prompt_template_config=prompt_template_config,
+    function_name = "function",
+    plugin_name = "plugin"
+)
+
+# Run your prompt
+# Note: functions are run asynchronously
+async def main():
+    result = await kernel.invoke(function)
+    print(result) # => Robots must not harm humans.
+
+if __name__ == "__main__":
+    asyncio.run(main())
