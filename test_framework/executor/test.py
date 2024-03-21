24287bc85cdf20782b00b8bd70143eb8554a891b
tianyangliao <alexliaovv@gmail.com>
2024-03-21 18:16:00 -0500

    venv activate command added

--- test_framework/executor/test.py
+++ test_framework/executor/test.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     # For MySQL
     mysql_executor = MySQLExecutor()
     mysql_executor.init('../config/mysql_config.json')
-    result = mysql_executor.execute_query('SELECT * FROM weather', 'bike_1')
+    result = mysql_executor.execute_query('SELECT * FROM weather LIMIT 10', 'bike_1')
     print(result)
 
     # For MongoDB
     mongodb_executor = MongoDBExecutor()
     mongodb_executor.init('../config/mongodb_config.json')
-    result = mongodb_executor.execute_query("{\"collection\": \"weather\", \"find\": {}}", "admin")
+    result = mongodb_executor.execute_query("{\"collection\": \"weather\", \"find\": {}, \"limit\": 10}", "admin")
     print(result)
