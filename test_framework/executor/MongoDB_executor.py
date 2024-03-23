8a86d12aa770cb2c37545e9f7d3bc558107af96f
BohanWu <819186192@qq.com>
2024-03-23 13:19:08 -0500

    feat: mongo on the fly executor

--- test_framework/executor/MongoDB_executor.py
+++ test_framework/executor/MongoDB_executor.py
@@ -15,18 +15,25 @@ class MongoDBExecutor(SQLExecutor):
         self.client = MongoClient(config['host'], config.get('port', 27017))
 
     def execute_query(self, query, database, schema):
-        json_object = json.loads(query)
         self.db = self.client[database]
-        collection = self.db[json_object['collection']]
         
-        if 'limit' in json_object:
-            result_cursor = collection.find(json_object['find']).limit(json_object['limit'])
-        else:
-            result_cursor = collection.find(json_object['find'])
+        exec_env = {
+            "db": self.db,
+        }
+
+        try:
+            adjusted_query = f'result = {query}'
+
+            exec(adjusted_query, exec_env)
+            records = exec_env.get("result")
+
+            results = []
+            for doc in records:
+                doc_json = {field: doc.get(field, None) for field in schema}
+                results.append(json.dumps(doc_json))
+
+            return results
+
+        except Exception as e:
+            return []
         
-        results = []
-        for doc in result_cursor:
-            doc_json = {field: doc.get(field, None) for field in schema}
-            results.append(json.dumps(doc_json))
-        
-        return results
