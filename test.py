71a52e0aa82596e19490be835a4b756428876ab6
feize <feizes2@illinois.edu>
2024-03-23 19:03:24 -0500

    catch exception

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from test_framework.executor.MongoDB_executor import MongoDBExecutor
 from test_framework.executor.MySQL_executor import MySQLExecutor
 from test_framework.comparator.hash import HashComparator
 if __name__ == '__main__':
-    sql_query = 'SELECT avg(lat) ,  avg(longitude) FROM station WHERE city  =  "San Jose"'
+    sql_query = 'SELECT avg(longitude) FROM station WHERE city  =  "San Jose"'
     mongo_query = 'db.station.aggregate([ { $match: { city: "San Jose" } }, { $group: { _id: null, avgLongitude: { $avg: "$longitude" } } }] )'
 
     mysql_executor = MySQLExecutor()
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     schema = mysql_executor.load_schema(sql_query, 'bike_1')
 
-    mysql_result = mysql_executor.execute_query(sql_query, 'bike_1', schema)
+    mysql_result, e = mysql_executor.execute_query(sql_query, 'bike_1', schema)
 
     mongodb_executor = MongoDBExecutor()
     mongodb_executor.init('./test_framework/config/mongodb_config.json')
