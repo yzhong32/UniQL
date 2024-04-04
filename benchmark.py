4a62f6eb7d66a605d8369af93d19e5efed86ebdb
Ubuntu <ubuntu@instance-20240227-234356.us-central1-a.c.cs511-415623.internal>
2024-04-04 22:26:42 +0000

    add try-except for executor

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,7 @@ from test_framework.executor.MongoDB_executor import *
 from test_framework.fetch.base import *
 from test_framework.comparator.hash import *
 from converter.convert import QueryConverter
-from contextlib import redirect_stdout
+from contextlib import redirect_stdout, redirect_stderr
 
 
 async def benchmark():
@@ -23,52 +23,49 @@ async def benchmark():
     success = 0
 
     with open('benchmark.log', 'w') as f:
-        with redirect_stdout(f):
+        with redirect_stdout(f), redirect_stderr(f):
             for (database, query) in query_fetcher.fetch_query("./query", "bike_1.json"):
             # for (database, query) in [('bike_1', 'SELECT * FROM station WHERE city  =  "San Jose"')]:
-                try: 
-                    if query in executed:
-                        continue
-                    executed.add(query)
-                    count += 1
-                    print(f"**********************SQL Query: {query}**************************")
-                    schema = mysql_executor.load_schema(query, database)
-                    print(schema)
+                if query in executed:
+                    continue
+                executed.add(query)
+                count += 1
 
-                    mysql_result, e = mysql_executor.execute_query(query, database, schema)
-                    if e is not None:
-                        print('execute mysql query error:{}'.format(e))
-                        continue
+                print(f"**********************SQL Query: {query}**************************")
+                schema = mysql_executor.load_schema(query, database)
+                print(schema)
 
-                    mongo_query = await convertor.convert(query)
-                    print(f"**********************MongoDB Query: {mongo_query}**************************")
-                    mongo_result, e = mongodb_executor.execute_query(mongo_query, database, schema)
-                    if e is not None:
-                        print('execute mongo query error:{}'.format(e))
-                        continue
+                mysql_result, e = mysql_executor.execute_query(query, database, schema)
+                if e is not None:
+                    print('execute mysql query error:{}'.format(e))
+                    continue
 
-                    matched_row, unmatched_row, e = comparator.compare(mysql_result, mongo_result)
-                    if unmatched_row == None or len(unmatched_row) != 0:
-                        print(query)
-                        print(e)
-                        print("mysql res:", mysql_result)
-                        print("mongo res:", mongo_result)
+                mongo_query = await convertor.convert(query)
+                print(f"**********************MongoDB Query: {mongo_query}**************************")
+
+                mongo_result, e = mongodb_executor.execute_query(mongo_query, database, schema)
+                if e is not None:
+                    print('execute mongo query error:{}'.format(e))
+                    continue
+
+                matched_row, unmatched_row, e = comparator.compare(mysql_result, mongo_result)
+                if unmatched_row == None or len(unmatched_row) != 0:
+                    print(query)
+                    print(e)
+                    print("mysql res:", mysql_result)
+                    print("mongo res:", mongo_result)
 
 
-                    if e is not None:
-                        print("Error comparing: {}".format(e))
-                        continue
+                if e is not None:
+                    print("Error comparing: {}".format(e))
+                    continue
 
-                    print('Matched: {}'.format(matched_row))
-                    print('Unmatched: {}'.format(unmatched_row))
+                print('Matched: {}'.format(matched_row))
+                print('Unmatched: {}'.format(unmatched_row))
 
-                    if len(unmatched_row) == 0:
-                        success += 1
-
-                except Exception as e:
-                    print(f"An error occurred: {e}")
+                if len(unmatched_row) == 0:
+                    success += 1
         
-
             print('Success: {}'.format(success))
             print('Total: {}'.format(count))
 
--- test_framework/executor/MySQL_executor.py
+++ test_framework/executor/MySQL_executor.py
@@ -13,34 +13,38 @@ class MySQLExecutor(SQLExecutor):
     def init(self, config_path):
         with open(config_path) as config_file:
             self.config = json.load(config_file)
+    
+    def get_conn(self, database):
+        self.connection = pymysql.connect(
+            host=self.config['host'],
+            user=self.config['user'],
+            password=self.config['password'],
+            database=database
+        )
 
     def execute_query(self, query, database, schema):
-        if self.connection is None or self.connection.db != database:
-            self.connection = pymysql.connect(
-                host=self.config['host'],
-                user=self.config['user'],
-                password=self.config['password'],
-                database=database
-            )
-        with self.connection.cursor() as cursor:
-            cursor.execute(query)
-            result = cursor.fetchall()
-            field_names = [convert_field_to_no_brace(desc[0]) for desc in cursor.description]
-            results = []
-            for row in result:
-                row_json = {field: row[field_names.index(field)] for field in schema if field in field_names}
-                results.append(simplejson.dumps(row_json))
-            return results, None
+        try:
+            if self.connection is None or self.connection.db != database:
+                self.get_conn(database)
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                field_names = [convert_field_to_no_brace(desc[0]) for desc in cursor.description]
+                results = []
+                for row in result:
+                    row_json = {field: row[field_names.index(field)] for field in schema if field in field_names}
+                    results.append(simplejson.dumps(row_json))
+                return results, None
+        except Exception as e:
+            return None, e
 
     def load_schema(self, query, database):
-        if self.connection == None or self.connection.db != database:
-            self.connection = pymysql.connect(
-                host=self.config['host'],
-                user=self.config['user'],
-                password=self.config['password'],
-                database=database
-            )
-        with self.connection.cursor() as cursor:
-            cursor.execute(query)
-            field_names = [convert_field_to_no_brace(i[0]) for i in cursor.description]
-            return field_names
+        try:
+            if self.connection == None or self.connection.db != database:
+                self.get_conn(database)
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
+                field_names = [convert_field_to_no_brace(i[0]) for i in cursor.description]
+                return field_names
+        except Exception as e:
+            return None, e
