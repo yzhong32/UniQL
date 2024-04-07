467f30a38ddb449b9b9daf0d42763787570a1b8e
yzhong32 <yzhong32@illinois.edu>
2024-04-07 12:58:03 -0500

    neo4j executor

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from test_framework.fetch.base import QueryFetcher
 from test_framework.executor.MongoDB_executor import MongoDBExecutor
 from test_framework.executor.MySQL_executor import MySQLExecutor
+from test_framework.executor.Neo4j_executor import Neo4jExecutor
 from test_framework.comparator.hash import HashComparator
+
 if __name__ == '__main__':
     sql_query = 'SELECT avg(longitude) FROM station WHERE city  =  "San Jose"'
     mongo_query = 'db.station.aggregate([ { $match: { city: "San Jose" } }, { $group: { _id: null, avgLongitude: { $avg: "$longitude" } } }] )'
+    neo4j_query = '''
+    MATCH (s:Station {city: "San Jose"}) RETURN avg(s.longitude) AS averageLongit
+    '''
 
     mysql_executor = MySQLExecutor()
     mysql_executor.init('./test_framework/config/mysql_config.json')
@@ -15,9 +20,17 @@ if __name__ == '__main__':
 
     mongodb_executor = MongoDBExecutor()
     mongodb_executor.init('./test_framework/config/mongodb_config.json')
-    mongo_result, e = mongodb_executor.execute_query(mongo_query, "bike_1", schema)
+    mongo_result, e = mongodb_executor.execute_query(mongo_query, "bike_1", ['avgLongitude'])
+
+    neo4j_executor = Neo4jExecutor()
+    neo4j_executor.init('./test_framework/config/neo4j_config.json')
+    neo4j_result, e = neo4j_executor.execute_query(neo4j_query)
+
+
     print(e)
     print(mysql_result)
-    print(mongo_result)
+    # print(mongo_result)
+    print(neo4j_result)
 
-    print(HashComparator().compare(mysql_result, mongo_result))
+    # print(HashComparator().compare(mysql_result, mongo_result))
+    print(HashComparator().compare(mysql_result, neo4j_result))
---
+++ test_framework/executor/Neo4j_executor.py
@@ -0,0 +1,39 @@
+import json
+from neo4j import GraphDatabase
+from .base import SQLExecutor
+
+class Neo4jExecutor(SQLExecutor):
+    def __init__(self):
+        self.driver = None
+
+    def init(self, config_path):
+        with open(config_path) as config_file:
+            config = json.load(config_file)
+        uri = config['uri']
+        user = config['user']
+        password = config['password']
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        if self.driver is not None:
+            self.driver.close()
+
+    def execute_query(self, query, database=None, schema=None):
+        session = self.driver.session(database=database) if database else self.driver.session()
+        try:
+            results = session.run(query)
+            formatted_results = []
+            for record in results:
+                if schema:
+                    record_json = {field: record.get(field) for field in schema}
+                else:
+                    record_json = dict(record.items())
+                formatted_results.append(json.dumps(record_json))
+            return formatted_results, None
+        except Exception as e:
+            return None, e
+        finally:
+            session.close()
+
+    def __del__(self):
+        self.close()
